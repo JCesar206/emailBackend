1dda9772076b0185da7cf337bfccf8827b236daf
Julio Cesar <jcesar206@hotmail.com>
2026-01-28 11:15:50 -0600

    Se actualiza archivo mail.routes para manejo de rutas y envio de correos con archivos adjuntos

--- src/routes/mail.routes.js
+++ src/routes/mail.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import auth from "../middlewares/auth.js";
-import { sendMail, inbox } from "../controllers/mail.controller.js";
+import { sendMail, inbox, mailDetail } from "../controllers/mail.controller.js";
 
 const router = Router();
-router.post("/", auth, sendMail);
+
+router.post("/", auth, upload.array("attachments"), sendMail);
 router.get("/", auth, inbox);
+router.get("/:id", auth, mailDetail);
 
 export default router;
