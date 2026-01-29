2831d3fcb98a3a985084f7d848c2f2a6e22798f6
Julio Cesar <jcesar206@hotmail.com>
2026-01-29 07:57:37 -0600

    Se actualiza logica del archivo upload.js para agregar archivos adjuntos

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -2,9 +2,14 @@ import multer from "multer";
 import path from "path";
 
 const storage = multer.diskStorage({
-	destination: "uploads/",
-	filename: (req, req, cb) => {
+	destination: (req, file, cb) => {
+		cb(null, "uploads/");
+	},
+	filename: (req, file, cb) => {
 		const uniqueName = Date.now() + "-" + file.originalname;
 		cb(null, uniqueName);
 	}
 });
+
+const upload = multer({ storage });
+export default upload;
