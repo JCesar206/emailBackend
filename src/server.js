e75bcc250e4c90dffeaa300a5606626572b49749
Julio Cesar <jcesar206@hotmail.com>
2026-02-02 14:33:15 -0600

    server.js: Se automatiza el server para aceptar el seed antes de levantar todas las conexiones con db

--- src/server.js
+++ src/server.js
@@ -1,10 +1,21 @@
 import "dotenv/config";
 import app from "./app.js";
 import logger from "./config/logger.js";
+import { initDb } from "./config/initDb.js";
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-	logger.info(`🚀 Servidor corriendo en http://localhost:${PORT}`);
-});
+async function startServer() {
+  try {
+    await initDb(); // DB primero
+    app.listen(PORT, () => {
+      logger.info(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    logger.error("❌ Error iniciando el servidor:", error.message);
+    process.exit(1);
+  }
+}
+
+startServer();
 // Punto único de arranque, Log claro al iniciar
